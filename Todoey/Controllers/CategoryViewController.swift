d38762d1a6ce47d1965ddb7bebe2e55b744d30d7
Ed Curran <ecurran@gmail.com>
2019-07-29 22:19:09 -0400

    Prior to adding functionality for Category View controller. So, up to 245...

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,52 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Ed Curran on 7/27/19.
+//  Copyright © 2019 Ed Curran. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //MARK: - TableView Datasource Methods
+        
+        //MARK: - Data Manipulation Methods
+        
+        
+        //MARK: - Add New Categories using addbuttonpressed.
+        
+        
+        
+        
+        
+        
+        
+
+      
+    }
+
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+        //MARK: - TableView Delegate Methods
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
