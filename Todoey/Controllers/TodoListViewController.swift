0dcf2e5651db888867d86e826b1ee4888917d23b
Ed Curran <ecurran@gmail.com>
2019-09-09 01:57:24 -0400

    finished 264 updating the UI of the navigation bar

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,13 +10,12 @@ import UIKit
 import RealmSwift
 import ChameleonFramework
 
-// Some New comments here.
-// an additional comment for source control testing.
-
 class TodoListViewController: SwipeTableViewController {
 
     var todoItems: Results<Item>?
     let realm = try! Realm()
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     
     var selectedCategory : Category? {
         didSet {
@@ -24,18 +23,55 @@ class TodoListViewController: SwipeTableViewController {
         }
     }
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+   let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     
-   // let defaults = UserDefaults.standard
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print(dataFilePath)
         
         tableView.separatorStyle = .none
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        title = selectedCategory!.name
+        
+        guard let colorHex = selectedCategory?.color else {fatalError() }
+            
+       updateNavBar(withHexCode: colorHex)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        updateNavBar(withHexCode: "1D9BF6")
+        
+    }
+   
+    //MARK: - Nav Bar Setup Methods
+    
+    func updateNavBar(withHexCode colorHexCode: String) {
+         guard let navBar = navigationController?.navigationBar else {fatalError("Navigation controller does not exist.")}
+        
+        guard let navBarColor = UIColor(hexString: colorHexCode) else {fatalError()}
+        
+        navBar.barTintColor = navBarColor
+        
+        navBar.tintColor = ContrastColorOf(navBarColor, returnFlat: true)
+        
+        navBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : ContrastColorOf(navBarColor, returnFlat: true)]
+        
+        searchBar.barTintColor = navBarColor
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 
     //MARK - Tableview Datasource Methods
     
